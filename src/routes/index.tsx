402dba54cbd0d2a298682fcff9402366466f84b2
Guilherme <guilherme.mendes@anbetec.com.br>
2024-11-11 10:50:14 -0300

    feat: adicionado responsividade do menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@mui/material'
 import {Route, Routes, Navigate} from 'react-router-dom'
-import { useAppThemeContext } from '../shared/contexts'
+import { useDrawerContext } from '../shared/contexts'
 
 export const AppRoutes = () => {
-    const {toggleTheme} = useAppThemeContext()
+    const {toggleDrawerOpen} = useDrawerContext()
     return (
         <Routes>
-            <Route path='/home' element={<Button variant='contained' color='primary' onClick={toggleTheme}>Página inicial</Button>}/>
+            <Route path='/home' element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle Drawer Open</Button>}/>
             <Route path='*'  element={<Navigate to='/home'/>}/>
         </Routes>
     )
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,17 +1,22 @@
+import React from 'react'
 import { Home } from "@mui/icons-material"
 import { Avatar, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
-import { Box } from "@mui/system"
+import { Box, useMediaQuery } from "@mui/system"
+import { useDrawerContext } from "../../contexts"
 
-interface IAppThemeProviderProps {
+interface IDrawerOpenProviderProps {
     children: React.ReactNode
 }
 
-export const MenuLateral: React.FC<IAppThemeProviderProps> = ({children}) => {
+export const MenuLateral: React.FC<IDrawerOpenProviderProps> = ({children}) => {
     const theme = useTheme()
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+
+    const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext()
 
     return(
     <>
-        <Drawer variant="permanent">
+        <Drawer open={isDrawerOpen} variant={smDown ? "temporary" : "permanent"} onClose={toggleDrawerOpen}>
             <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
                 <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
                     <Avatar sx={{height: theme.spacing(12), width: theme.spacing(12)}} src="https://play-lh.googleusercontent.com/hwqIBbPBOLGIxwciuns6_P8n3FSUPXmEyxwREou_AxK4q_lP6r825JWxG2l6X5OOgnV7" />
@@ -29,7 +34,7 @@ export const MenuLateral: React.FC<IAppThemeProviderProps> = ({children}) => {
                 </Box>
             </Box>
         </Drawer>
-        <Box height='100vh' marginLeft={theme.spacing(28)}>
+        <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
             {children}
         </Box>
         
