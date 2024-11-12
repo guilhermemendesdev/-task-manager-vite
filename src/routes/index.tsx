3900d567b9cc716641c16851a421e400909ed4d0
Guilherme <guilherme.mendes@anbetec.com.br>
2024-11-12 12:33:10 -0300

    feat: inicialização de rotas para o modulo de tasks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import {Route, Routes, Navigate} from 'react-router-dom'
 import { useDrawerContext } from '../shared/contexts'
 import { useEffect } from 'react'
-import { Home, People } from '@mui/icons-material'
+import { Home, People, Task } from '@mui/icons-material'
 import { Dashboard, ListagemDeDevelopers } from '../pages'
 import { NovoDeveloper } from '../pages/developers/NovoDeveloper'
+import { ListagemDeTasks } from '../pages/tasks/ListagemDeTasks'
+import { NovaTask } from '../pages/tasks/NovaTask'
 
 export const AppRoutes = () => {
     const {setDrawerOptions} = useDrawerContext()
@@ -19,6 +21,11 @@ export const AppRoutes = () => {
                 path: 'developers',
                 icon: <People/>,
                 label: 'Developers'
+            },
+            {
+                path: 'tasks',
+                icon: <Task/>,
+                label: 'Tarefas'
             }
         ])
     }, [])
@@ -28,6 +35,9 @@ export const AppRoutes = () => {
             <Route path='/home' element={<Dashboard/>}/>
             <Route path='/developers' element={<ListagemDeDevelopers/>}/>
             <Route path='/developers/novo' element={<NovoDeveloper/>}/>
+
+            <Route path='/tasks' element={<ListagemDeTasks/>}/>
+            <Route path='/tasks/nova' element={<NovaTask/>}/>
             <Route path='*'  element={<Navigate to='/home'/>}/>
         </Routes>
     )
